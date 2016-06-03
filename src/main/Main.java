8c97b45de3ce27a507d8070ccea0481ee72a1701
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2016-06-03 13:30:56 +0200

    Imagenes frame

---
+++ src/main/Main.java
@@ -0,0 +1,11 @@
+
+package main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        frame.frame pantalla = new frame.frame();
+        pantalla.setVisible(true);
+    }
+    
+}
